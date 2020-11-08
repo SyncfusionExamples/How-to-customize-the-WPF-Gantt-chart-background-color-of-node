ad82703198931cfa81897f83e99a705e084b02ea
Hemalatha <hemalathavinai@gmail.com>
2020-11-08 22:38:04 +0530

    sample has been included

---
+++ Gantt_GettingStarted/Behavior/GanttNodeCustomizationBehavior.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Gantt_GettingStarted
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+    using System.Windows.Media;
+
+    class GanttNodeCustomizationBehavior:Behavior<Border>
+    {
+        public static Random r = new Random();
+
+        public static string[] nodeColors = new string[]
+                                                {
+                                                    "#FFFF0094", "#FFA500FF", "#FF00AAAD", "#FF8CBE21", "#FFA55100",
+                                                    "#FFE771BD", "#FFF79608", "#FF18A2E7", "#FFE71400", "#FF319A31"
+                                                };
+
+        /// <summary>
+        /// Called when [attached].
+        /// </summary>
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.Loaded += new RoutedEventHandler(AssociatedObject_Loaded);
+        }
+
+        /// <summary>
+        /// Handles the Loaded event of the AssociatedObject control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            var name = ((e.OriginalSource as Border).DataContext as Item).Name;
+            Border node = sender as Border;
+            if (name == "Design Spec")
+            {
+                node.Background = (Brush)new BrushConverter().ConvertFromString(nodeColors[0]);
+                node.BorderBrush = node.Background;
+            }
+            else if (name == "Requirement Spec")
+            {
+                node.Background = (Brush)new BrushConverter().ConvertFromString(nodeColors[1]);
+                node.BorderBrush = new SolidColorBrush(Colors.Black); ;
+            }
+            else if (name == "Defining UI Design")
+            {
+                node.Background = (Brush)new BrushConverter().ConvertFromString(nodeColors[2]);
+                node.BorderBrush = new SolidColorBrush(Colors.Black); ;
+            }
+            else if (name == "Completing Overall Graphics design")
+            {
+                node.Background = (Brush)new BrushConverter().ConvertFromString(nodeColors[3]);
+                node.BorderBrush = new SolidColorBrush(Colors.Black); ;
+            }
+            else if (name == "Development Plan")
+            {
+                node.Background = (Brush)new BrushConverter().ConvertFromString(nodeColors[4]);
+                node.BorderBrush = new SolidColorBrush(Colors.Black); ;
+            }
+            else if (name == "Self Testing")
+            {
+                node.Background = (Brush)new BrushConverter().ConvertFromString(nodeColors[5]);
+                node.BorderBrush = new SolidColorBrush(Colors.Black); ;
+            }
+            
+        }
+
+        /// <summary>
+        /// Called when [detaching].
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.Loaded -= new RoutedEventHandler(AssociatedObject_Loaded);
+        }
+    }
+}
---
+++ Gantt_GettingStarted/ViewModel/ViewModel.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Gantt_GettingStarted
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// View model class.
+    /// </summary>
+    public class ViewModel
+    {
+        /// <summary>
+        /// private field for team details
+        /// </summary>
+        private ObservableCollection<Item> teamDetails;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModel"/> class.
+        /// </summary>
+        public ViewModel()
+        {
+            this.teamDetails = this.GetTeamInfo();
+        }
+
+        /// <summary>
+        /// Gets or sets the appointment item source.
+        /// </summary>
+        /// <value>The appointment item source.</value>
+        public ObservableCollection<Item> TeamDetails
+        {
+            get
+            {
+                return this.teamDetails;
+            }
+
+            set
+            {
+                this.teamDetails = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the team info.
+        /// </summary>
+        /// <returns>returns the team info</returns>
+        public ObservableCollection<Item> GetTeamInfo()
+        {
+            ObservableCollection<Item> teams = new ObservableCollection<Item>();
+
+            teams.Add(new Item() { Name = "RDU Team" });
+            Item Person = new Item() { Name = "Robert" };
+            Person.InLineItems.Add(new Item() { StartDate = new DateTime(2012, 01, 17, 12, 0, 0), FinishDate = new DateTime(2012, 01, 21), Name = "Design Spec" });
+            teams[0].SubItems.Add(Person);
+
+            Person = new Item() { Name = "Michael" };
+            Person.InLineItems.Add(new Item() { StartDate = new DateTime(2012, 01, 19, 12, 0, 0), FinishDate = new DateTime(2012, 01, 23), Name = "Requirement Spec" });
+            teams[0].SubItems.Add(Person);
+
+
+
+            teams.Add(new Item() { Name = "Graphics Team" });
+            Person = new Item() { Name = "Madhan" };
+            Person.InLineItems.Add(new Item() { StartDate = new DateTime(2012, 01, 21, 12, 0, 0), FinishDate = new DateTime(2012, 01, 26), Name = "Defining UI Design" });
+            teams[1].SubItems.Add(Person);
+
+            Person = new Item() { Name = "Peter" };
+            Person.InLineItems.Add(new Item() { StartDate = new DateTime(2012, 01, 24, 12, 0, 0), FinishDate = new DateTime(2012, 01, 28), Name = "Completing Overall Graphics design" });
+            teams[1].SubItems.Add(Person);
+
+
+            teams.Add(new Item() { Name = "Dev Team" });
+            Person = new Item() { Name = "Ruban" };
+            Person.InLineItems.Add(new Item() { StartDate = new DateTime(2012, 01, 26, 12, 0, 0), FinishDate = new DateTime(2012, 01, 30), Name = "Development Plan", Progress = 10 });
+            teams[2].SubItems.Add(Person);
+
+            Person = new Item() { Name = "Karthick" };
+            Person.InLineItems.Add(new Item() { StartDate = new DateTime(2012, 01, 29, 12, 0, 0), FinishDate = new DateTime(2012, 02, 2), Name = "Self Testing" });
+            teams[2].SubItems.Add(Person);
+
+
+            return teams;
+        }
+    }
+}
